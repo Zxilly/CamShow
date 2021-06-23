f9c765ae5af78bd36eeb7ec6d1719df5c603d481
Zxilly <zhouxinyu1001@gmail.com>
2021-06-24 00:42:51 +0800

    add lazyload support

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ error_reporting(0);
                             $photodes=$data[2];
                             echo <<<EOF
 <article class="thumb">
-<a href="images/$filename" class="image"><img src="images/$filename" alt="" /></a>
+<a href="images/$filename" class="image"><img loading=lazy src="images/$filename" alt="" /></a>
 <h2>$photoname</h2>
 <p>$photodes</p>
 </article>
